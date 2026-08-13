958ab5fc4b4980c9763c698c363ccdbdbc9bb569
Gabriel  Barreto Otero <120350521+Luxx12@users.noreply.github.com>
2026-08-12 22:31:57 -0400

    Update parser.cpp

--- compiler/parser.cpp
+++ compiler/parser.cpp
@@ -1,5 +1,4 @@
 #include "parser.h"
-
 #include <algorithm>
 
 Token Parser::peek() {
@@ -23,6 +22,234 @@ bool Parser::match(const tokenType& type) {
         return true;
     }
     return false;
+}
+
+std::unique_ptr<ASTNode> Parser::parseStatement() {
+
+    if (peek().lexeme == "if") {
+        return parseIfStmt();
+    }
+    else if (peek().lexeme == "for") {
+        return parseForLoop();
+    }
+    else if (peek().lexeme == "while") {
+        return parseWhileLoop();
+    }
+    else if (peek().lexeme == "return") {
+        return parseReturn();
+    }
+    else if (peek().lexeme == "{") {
+        return parseBlock();
+    }
+    else if (peek().lexeme == ";") {
+
+    }
+    else if (peek().type == tokenType::DATATYPE) {
+        return determineDeclaration();
+    }
+    else {
+        return parseExprStmt();
+    }
+}
+
+std::unique_ptr<ASTNode> Parser::parseReturn() {
+    std::unique_ptr<ASTNode> returnNode = std::make_unique<ASTNode>();
+    std::unique_ptr<ASTNode> returnValue;
+
+    advance(); // consume return key word
+
+    returnValue = parseAssignment();
+
+    returnNode->value = ReturnNode(std::move(returnValue));
+
+    if (!match(tokenType::SEMICOLON)) throw std::runtime_error("Missing semicolon");
+
+    return returnNode;
+}
+
+std::unique_ptr<ASTNode> Parser::parseIfStmt() {
+    std::unique_ptr<ASTNode> ifNode = std::make_unique<ASTNode>();
+    std::unique_ptr<ASTNode> conditionNode, blockNode;
+
+    // consume 'if' keyword and '('
+    if (peek().lexeme != "if") throw std::runtime_error("Incorrect if loop syntax.");
+    advance();
+
+    if (!match(tokenType::LPARANTHESIS)) throw std::runtime_error("Incorrect if loop syntax.");
+
+    // parse condition, consume ']' & '{' then parse if statement's block
+    conditionNode = parseAssignment();
+
+    if (!match(tokenType::RPARANTHESIS)) throw std::runtime_error("Incorrect if loop syntax.");
+
+    blockNode = parseBlock();
+
+    ifNode->value = IfNode(std::move(conditionNode), std::move(blockNode));
+
+    return ifNode;
+}
+
+std::unique_ptr<ASTNode> Parser::parseWhileLoop() {
+    std::unique_ptr<ASTNode> whileNode = std::make_unique<ASTNode>();
+    std::unique_ptr<ASTNode> conditionNode, blockNode;
+
+    // consume 'while' keyword and '('
+    if (peek().lexeme != "while") throw std::runtime_error("Incorrect while loop syntax.");
+    advance();
+
+    if (!match(tokenType::LPARANTHESIS)) throw std::runtime_error("Incorrect while loop syntax.");
+
+    // parse condition, consume ')' & '{' then parse while loop's block
+    conditionNode = parseAssignment();
+
+    if (!match(tokenType::RPARANTHESIS)) throw std::runtime_error("Incorrect while loop syntax.");
+
+    blockNode = parseBlock();
+
+    whileNode->value = WhileNode(std::move(conditionNode), std::move(blockNode));
+
+    return whileNode;
+}
+
+std::unique_ptr<ASTNode> Parser::parseForLoop() {
+    std::unique_ptr<ASTNode> forNode = std::make_unique<ASTNode>();
+    std::unique_ptr<ASTNode> initNode, conditionNode, incNode, blockNode;
+
+    // consume 'for' keyword and '('
+    if (peek().lexeme != "for") throw std::runtime_error("Incorrect for loop syntax.");
+    advance();
+
+    if (!match(tokenType::LPARANTHESIS)) throw std::runtime_error("Incorrect for loop syntax.");
+
+    // parse initliization, condition, & increment. consume ';' when needed
+    initNode = parseAssignment();
+    if (!match(tokenType::SEMICOLON)) throw std::runtime_error("Incorrect for loop syntax.");
+    conditionNode = parseAssignment();
+    if (!match(tokenType::SEMICOLON)) throw std::runtime_error("Incorrect for loop syntax.");
+    incNode = parseAssignment();
+
+    if (!match(tokenType::RPARANTHESIS)) throw std::runtime_error("Incorrect while loop syntax.");
+
+    blockNode = parseBlock();
+
+    forNode->value = ForNode(std::move(initNode),std::move(conditionNode), std::move(incNode), std::move(blockNode));
+
+    return forNode;
+}
+std::unique_ptr<ASTNode> Parser::determineDeclaration() {
+    std::unique_ptr<ASTNode> dataTypeNode = parseDataType();
+
+    // determines which node to build depending on whether the current token is a function or var identifier
+    if (peek().type == tokenType::IDENTIFIER) {
+        std::unique_ptr<ASTNode> varDecNode = parseVarDeclaration(std::move(dataTypeNode));
+
+        return varDecNode;
+    }
+    else {
+        std::unique_ptr<ASTNode> funcSignatureNode = parseFunctionDeclaration(std::move(dataTypeNode));
+
+        return funcSignatureNode;
+    }
+}
+
+std::unique_ptr<ASTNode> Parser::parseVarDeclaration(std::unique_ptr<ASTNode> dataTypeNode) {
+    std::unique_ptr<ASTNode> decNode = std::make_unique<ASTNode>();
+
+    std::unique_ptr<ASTNode> assignmentNode = parseExprStmt();
+
+    decNode->value = DeclarationNode(std::move(dataTypeNode),std::move(assignmentNode));
+
+    return decNode;
+}
+
+std::unique_ptr<ASTNode> Parser::parseFunctionDeclaration(std::unique_ptr<ASTNode> dataTypeNode) {
+    std::vector<std::unique_ptr<ASTNode>> parameters = {};
+    std::unique_ptr<ASTNode> funcNode = std::make_unique<ASTNode>();
+    std::unique_ptr<ASTNode> funcIdentifer = parsePrimary();
+    std::unique_ptr<ASTNode> paramDataType, paramVarRef;
+
+    funcNode->value = FunctionDecNode(std::move(dataTypeNode), std::move(funcIdentifer), {}, nullptr);
+    FunctionDecNode& function = std::get<FunctionDecNode>(funcNode->value);
+
+    // consume '('
+    if (!match(tokenType::LPARANTHESIS)) throw std::runtime_error("incorrect function signature syntax");
+
+    // parse parameters
+    while (!match(tokenType::RPARANTHESIS)) {
+        paramDataType = parseDataType();
+        paramVarRef = parsePrimary();
+
+        std::unique_ptr<ASTNode> parameterNode = std::make_unique<ASTNode>();
+        parameterNode->value = ParameterNode(std::move(paramDataType), std::move(paramVarRef));
+
+        function.parameters.push_back(std::move(parameterNode));
+        match(tokenType::COMMA);
+    }
+
+    function.block = parseBlock();
+
+    return funcNode;
+}
+
+std::unique_ptr<ASTNode> Parser::parseFunctionCall(std::unique_ptr<ASTNode> funcIdentifier) {
+    std::unique_ptr<ASTNode> funcNode = std::make_unique<ASTNode>();
+    std::unique_ptr<ASTNode> funcIdentifer = std::move(funcIdentifer);
+    std::unique_ptr<ASTNode> paramVarRef;
+
+    funcNode->value = FunctionCallNode(std::move(funcIdentifer), {});
+    FunctionCallNode& function = std::get<FunctionCallNode>(funcNode->value);
+
+    // consume '('
+    if (!match(tokenType::LPARANTHESIS)) throw std::runtime_error("incorrect function signature syntax");
+
+    // parse parameters
+    while (!match(tokenType::RPARANTHESIS)) {
+        paramVarRef = parsePrimary();
+
+        function.parameters.push_back(std::move(paramVarRef));
+        match(tokenType::COMMA);
+    }
+
+    return funcNode;
+}
+
+std::unique_ptr<ASTNode> Parser::parseExprStmt() {
+    std::unique_ptr<ASTNode> exprStmt = parseAssignment();
+
+    // consume ';' if it exists, throws an error if not
+    if (!match(tokenType::SEMICOLON)) {
+        throw std::runtime_error("No semicolon.");
+    }
+
+    return exprStmt;
+}
+
+std::unique_ptr<ASTNode> Parser::parseBlock() {
+    std::unique_ptr<ASTNode> block = std::make_unique<ASTNode>();
+    block->value = BlockNode({});
+    BlockNode& curBlock = std::get<BlockNode>(block->value);
+
+
+    // consume '{'
+    if (!match(tokenType::LCURLYBRACKET)) throw std::runtime_error("Incorrect function block syntax");
+
+    // continously parse each statement within the block until the block/file ends
+    while (peek().lexeme != "}" && peek().type != tokenType::END) {
+        curBlock.statements.push_back(parseStatement());
+    }
+
+    // consume '}'
+    if (!match(tokenType::RCURLYBRACKET)) throw std::runtime_error("Incorrect function block syntax");
+
+    return block;
+}
+
+std::unique_ptr<ASTNode> Parser::parseDataType() {
+    std::unique_ptr<ASTNode> dataType = std::make_unique<ASTNode>();
+    dataType->value = DataTypeNode(peek().lexeme);
+    advance();
+
+    return dataType;
 }
 
 std::unique_ptr<ASTNode> Parser::parseAssignment() {
@@ -51,9 +278,26 @@ std::unique_ptr<ASTNode> Parser::parseAssignment() {
 }
 
 std::unique_ptr<ASTNode> Parser::parseEquality() {
+    std::unique_ptr<ASTNode> left = parseRelational();
+
+    while (peek().lexeme == "==" || peek().lexeme == "!=") {
+        Token op = peek();
+        advance();
+
+        std::unique_ptr<ASTNode> right = parseRelational();
+
+        std::unique_ptr<ASTNode> subTree = std::make_unique<ASTNode>();
+        subTree->value = BinaryExprNode(op,std::move(left),std::move(right));
+        left = std::move(subTree);
+    }
+
+    return left;
+}
+
+std::unique_ptr<ASTNode> Parser::parseRelational() {
     std::unique_ptr<ASTNode> left = parseShift();
 
-    while (peek().lexeme == "==") {
+    while (peek().lexeme == ">=" || peek().lexeme == "<=" || peek().lexeme == ">" || peek().lexeme == "<") {
         Token op = peek();
         advance();
 
@@ -66,7 +310,6 @@ std::unique_ptr<ASTNode> Parser::parseEquality() {
 
     return left;
 }
-
 std::unique_ptr<ASTNode> Parser::parseShift() {
     std::unique_ptr<ASTNode> left = parseAdditive();
 
@@ -140,7 +383,18 @@ std::unique_ptr<ASTNode> Parser::parseUnary() {
     if (newTree) {
         return newTree;
     }
-    return parsePrimary();
+    return determineFunctionCall();
+}
+
+std::unique_ptr<ASTNode> Parser::determineFunctionCall() {
+    std::unique_ptr<ASTNode> functionCall = std::make_unique<ASTNode>();
+    std::unique_ptr<ASTNode> funcIdentifer = parseAssignment();
+
+    if (peek().type == tokenType::LPARANTHESIS) {
+        return parseFunctionCall(std::move(funcIdentifer));
+    }
+
+    return funcIdentifer;
 }
 
 std::unique_ptr<ASTNode> Parser::parsePrimary() {
@@ -149,7 +403,7 @@ std::unique_ptr<ASTNode> Parser::parsePrimary() {
     std::unique_ptr<ASTNode> primNode = std::make_unique<ASTNode>();
 
     // checks if token is of a primary type and assigns to the primary node as AST Node that varies on its AST type
-    // depending on what the token is, if its not a primary then an exception is thrown
+    // depending on what the token is, if it's not a primary then an exception is thrown
     if (match(tokenType::STRING)) {
         primNode->value = StringNode(t.lexeme);
     }
@@ -158,6 +412,9 @@ std::unique_ptr<ASTNode> Parser::parsePrimary() {
     }
     else if (match(tokenType::IDENTIFIER)) {
         primNode->value = VarRefNode(t.lexeme);
+    }
+    else if (match(tokenType::FUNCTION)) {
+        primNode->value = FuncIdentifier(t.lexeme);
     }
     else {
         throw std::runtime_error("Expected an expression.");
@@ -185,6 +442,9 @@ void Parser::printAST(const ASTNode& node, int depth) {
             std::cout << indent << "UnaryExpr(" << n.op.lexeme << ")\n";
             printAST(*n.value, depth + 1);
         }
+        else if constexpr(std::is_same_v<T, DataTypeNode>) {
+            std::cout << indent << "Data Type(" << n.dataType << ")\n";
+        }
         else if constexpr (std::is_same_v<T, BinaryExprNode>) {
             std::cout << indent << "BinaryExpr(" << n.op.lexeme << ")\n";
             std::cout << indent << "  left:\n";
@@ -199,11 +459,70 @@ void Parser::printAST(const ASTNode& node, int depth) {
             std::cout << indent << "  value:\n";
             printAST(*n.value, depth + 2);
         }
+        else if constexpr (std::is_same_v<T, BlockNode>) {
+            std::cout << indent << " Block:\n";
+            for (size_t i = 0; i < n.statements.size(); i++) {
+                printAST(*n.statements[i], depth + 2);
+            }
+        }
+        else if constexpr(std::is_same_v<T, WhileNode>) {
+            std::cout << indent << "While Loop\n";
+            std::cout << indent << "  condition:\n";
+            printAST(*n.condition, depth + 2);
+            std::cout << indent << "  block:\n";
+            printAST(*n.block, depth + 2);
+        }
+        else if constexpr(std::is_same_v<T, IfNode>) {
+            std::cout << indent << "If Statement\n";
+            std::cout << indent << "  condition:\n";
+            printAST(*n.condition, depth + 2);
+            std::cout << indent << "  block:\n";
+            printAST(*n.block, depth + 2);
+        }
+        else if constexpr(std::is_same_v<T, ForNode>) {
+            std::cout << indent << "For Loop\n";
+            std::cout << indent << "  initialization:\n";
+            printAST(*n.initialization, depth + 2);
+            std::cout << indent << "  condition:\n";
+            printAST(*n.condition, depth + 2);
+            std::cout << indent << "  increment:\n";
+            printAST(*n.increment, depth + 2);
+            std::cout << indent << "  block:\n";
+            printAST(*n.block, depth + 2);
+        }
+        else if constexpr(std::is_same_v<T, DeclarationNode>) {
+            std::cout << indent << "Variable Declaration\n";
+            std::cout << indent << "  DataType:\n";
+            printAST(*n.dataType, depth + 2);
+            std::cout << indent << "  Variable Assignment:\n";
+            printAST(*n.varAssignment, depth + 2);
+        }
+        else if constexpr(std::is_same_v<T,ParameterNode>) {
+            std::cout << indent << "Parameter\n";
+            std::cout << indent << "  DataType:\n";
+            printAST(*n.dataType, depth + 2);
+            std::cout << indent << "  Variable Assignment:\n";
+            printAST(*n.varAssignment, depth + 2);
+        }
+        else if constexpr(std::is_same_v<T, FunctionDecNode>) {
+            std::cout << indent << "Function Declaration\n";
+            std::cout << indent << "  ReturnType:\n";
+            printAST(*n.returnType, depth + 2);
+            std::cout << indent << "  Identifier:\n";
+            printAST(*n.funcIdentifer, depth + 2);
+            std::cout << indent << "  Parameters:\n";
+            for (size_t i = 0; i < n.parameters.size(); i++) {
+                printAST(*n.parameters[i], depth + 2);
+            }
+            std::cout << indent << "  Block:\n";
+            printAST(*n.block, depth + 2);
+        }
+
     }, node.value);
 }
 
 std::unique_ptr<ASTNode> Parser::parse(std::vector<Token>& inputtedTokens) {
     tokens = inputtedTokens;
     current = 0;
-    return parseAssignment();
+    return parseStatement();
 }
